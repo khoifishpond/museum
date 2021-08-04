daaff7157a8a9a4a8b4064d693e48c3ebd2fadfd
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-04 13:31:34 -0700

    Add Interests context and test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -23,4 +23,17 @@ describe Patron do
       expect(@patron_1.interests).to eq([])
     end
   end
+
+  context 'Add Interests' do
+    before(:each) do
+      @patron_1 = Patron.new("Bob", 20)
+    end
+
+    it 'can add interests' do
+      @patron_1.add_interest("Dead Sea Scrolls")
+      @patron_1.add_interest("Gems and Minerals")
+
+      expect(@patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+    end
+  end
 end
